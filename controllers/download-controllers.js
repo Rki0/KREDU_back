52d7132c2449ab49dc4f4aa62baac08df30e8ce5
Rki0 <pky00823@gmail.com>
2023-02-12 22:29:47 +0900

    feat(download-controllers): add function to download file

---
+++ controllers/download-controllers.js
@@ -0,0 +1,38 @@
+const fs = require("fs");
+const HttpError = require("../models/http-error");
+
+const downloadFile = async (req, res, next) => {
+  const fileName = req.params.fileName;
+
+  let isFileExist;
+
+  try {
+    isFileExist = fs.existsSync(`uploads/attachments/${fileName}`);
+  } catch (err) {
+    const error = new HttpError(
+      "File searching is failed...Please try again.",
+      500
+    );
+
+    next(error);
+  }
+
+  if (!isFileExist) {
+    const error = new HttpError("There is no file. Please try again.", 500);
+
+    next(error);
+  }
+
+  try {
+    res.download(`uploads/attachments/${fileName}`);
+  } catch (err) {
+    const error = new HttpError(
+      "File download is faild. Please try again.",
+      500
+    );
+
+    next(error);
+  }
+};
+
+exports.downloadFile = downloadFile;
