9431b55f4a1eee090dc027bd8d633e48756cc8a8
Rki0 <pky00823@gmail.com>
2023-02-14 22:35:01 +0900

    feat(User): add base of data model for user

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+  nickname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  image: {
+    type: String,
+  },
+  role: {
+    type: Number,
+  },
+  likes: [
+    {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "Lecture",
+    },
+  ],
+  lectureComments: [
+    {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "LectureComment",
+    },
+  ],
+  lectureSubComments: [
+    {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: "LectureSubComment",
+    },
+  ],
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
